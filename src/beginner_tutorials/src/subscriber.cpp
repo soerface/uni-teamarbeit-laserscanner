0d769ca0e324ff9a8fb83b942fcd2128879bec6a
BZeidler <b.zeidler@yahoo.de>
2016-06-09 14:21:07 +0200

    erster Commit, gibt einen Wert des Scanners aus

---
+++ src/beginner_tutorials/src/subscriber.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "ros/spinner.h"
+#include <iostream>
+
+void chatterCallback(sensor_msgs::LaserScanPtr msg)
+{
+    std::cout<<"in callback"<< std::endl;
+    ROS_INFO("I heard: [%f]", msg->ranges[1]);
+}
+
+int main(int argc, char **argv)
+{
+//   std::cout<<"test"<<std::endl;
+    ros::init(argc, argv, "subscriber");
+    ros::NodeHandle *n = new ros::NodeHandle();
+    ros::Subscriber sub = n->subscribe("scan", 1000, chatterCallback);
+
+    if(sub){
+        std::cout<<sub<<std::endl;
+        ros::AsyncSpinner *spinner = new ros::AsyncSpinner(3);
+        spinner->start();
+        while(ros::ok())
+        {
+            sleep(1);
+        }
+        sub.shutdown();
+        delete sub;
+    }
+    else
+    {
+        std::cout<<"no sub"<<std::endl;
+    }
+
+  return 0;
+}
